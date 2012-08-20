8331383b2a98f17e1d1170112854fddf3a280def
Alex Palcuie <alex.palcuie@gmail.com>
2012-08-20 23:40:56 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeoFun::Application.config.secret_token = '857692e83241d74bfbb2e3d26112f8a9b7f2c891819148c50766d642b898452e8a78f7f4207fe8358762c4e2e42416a5cd64330d93a0487116a2641e9c91033e'
